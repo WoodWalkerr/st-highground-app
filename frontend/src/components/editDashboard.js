bf04a6e431234fe0f56f5e7a16867391280dba75
Melven Bagolaque <melvenbagolaque@gmail.com>
2023-04-07 05:26:15 +0800

    fix: changes in components

--- frontend/src/components/editDashboard.js
+++ frontend/src/components/editDashboard.js
@@ -1,88 +1,115 @@
 import React, { Fragment, useState } from 'react'
 
 const EditDashboard = ({ user }) => {
-    const [users, setUser] = useState(user?.users || '')
+  const [name, setName] = useState(user?.name || '')
+  const [email, setEmail] = useState(user?.email || '')
+  const [phoneNumber, setPhoneNumber] = useState(user?.phoneNumber || '')
+  const [type, setType] = useState(user?.type || '')
 
-    // edit description
-    const updateUser = async (e) => {
-        e.preventDefault()
-        try {
-            const body = { users }
-            const response = await fetch(
-                `http://localhost:8080/api/v1/users/${user.id}`,
-                {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(body),
-                }
-            )
-
-            window.location = '/'
-        } catch (error) {
-            console.error(error.message)
+  // update user information
+  const updateUser = async (e) => {
+    e.preventDefault()
+    try {
+      const body = { name, email, phoneNumber, type }
+      const response = await fetch(
+        `http://localhost:8080/api/v1/users/${user.id}`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
         }
+      )
+      window.location = '/'
+    } catch (error) {
+      console.error(error.message)
     }
+  }
 
-    const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false)
 
-    const toggleModal = () => {
-        setShowModal(!showModal)
-        setUser(user?.id || '')
-    }
+  const toggleModal = () => {
+    setShowModal(!showModal)
+    setName(user?.name || '')
+    setEmail(user?.email || '')
+    setPhoneNumber(user?.phoneNumber || '')
+    setType(user?.type || '')
+  }
 
-    return (
-        <Fragment>
-            {/* Button to open the modal */}
-            <div className="flex justify-center items-center py-4 mx-10">
-                <button
-                    className="bg-green-500 rounded-md text-white w-20 h-10 from-green-500 to-green-800 font-normal"
-                    onClick={toggleModal}
-                >
-                    Edit
-                </button>
-            </div>
+  return (
+    <Fragment>
+      {/* Button to open the modal */}
+      <div className="flex justify-center items-center py-4 mx-10">
+        <button
+          className="bg-green-500 rounded-md text-white w-20 h-10 from-green-500 to-green-800 font-normal"
+          onClick={toggleModal}
+        >
+          Edit
+        </button>
+      </div>
 
-            {/* Modal code */}
-            {showModal && (
-                <div className="fixed z-10 inset-0 overflow-y-auto">
-                    <div className="flex items-center justify-center min-h-screen">
-                        <div className="bg-white rounded-lg">
-                            <div className="p-4">
-                                <h2 className="text-lg font-bold mb-4">
-                                    Edit users
-                                </h2>
-                                <div className="mb-4">
-                                    <input
-                                        type="text"
-                                        placeholder="Edit here"
-                                        value={users}
-                                        onChange={(e) =>
-                                            setUser(e.target.value)
-                                        }
-                                    />
-                                </div>
-                                <button
-                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                                    onClick={(e) => {
-                                        toggleModal()
-                                        updateUser(e)
-                                    }}
-                                >
-                                    Edit
-                                </button>
-                                <button
-                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                                    onClick={toggleModal}
-                                    onChange={() => setUser(user.id)}
-                                >
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+      {/* Modal code */}
+      {showModal && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-white rounded-lg">
+              <div className="p-4">
+                <h2 className="text-lg font-bold mb-4">Edit user information</h2>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
-            )}
-        </Fragment>
-    )
+                <div className="mb-4">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="tel"
+                    placeholder="Phone number"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    placeholder="Type"
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <button
+                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={(e) => {
+                      toggleModal()
+                      updateUser(e)
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={toggleModal}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  )
 }
-export default EditDashboard
+
+export default EditDashboard;
--- frontend/src/components/inputDashboard.js
+++ frontend/src/components/inputDashboard.js
@@ -1,43 +1,96 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react'
 
 const InputDashboard = () => {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        phone_number: '',
+        type: '',
+    })
 
-  const [user, setUser] = useState("");
+    const { name, email, password, phone_number, type } = user
 
-  const onSubmitForm = async (e) => {
-    e.preventDefault()
-    try {
-      const body = { user };
-      const response = await fetch("http://localhost:8080/api/v1/users", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
-      });
-
-    window.location = "/";
-    } catch (error) {
-      console.error(error.message)
+    const onSubmitForm = async (e) => {
+      e.preventDefault()
+      if (!name || !email || !password || !phone_number || !type) {
+        alert('Please fill in all fields')
+        return
+      }
+      try {
+        const body = { user }
+        const response = await fetch('http://localhost:8080/api/v1/users', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        })
+    
+        window.location = '/'
+      } catch (error) {
+        console.error(error.message)
+      }
     }
-  };
+    
+    return (
+        <Fragment>
+            <h1 className="text-center font-bold text-4xl">High Ground App</h1>
+            <div className="flex justify-center items-center">
+                <form
+                    className="flex flex-col w-[35%] justify-center my-4"
+                    onSubmit={onSubmitForm}
+                >
+                    <input
+                        type="text"
+                        className="py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) =>
+                            setUser({ ...user, name: e.target.value })
+                        }
+                    />
+                    <input
+                        type="email"
+                        className="py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) =>
+                            setUser({ ...user, email: e.target.value })
+                        }
+                    />
+                    <input
+                        type="password"
+                        className="py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) =>
+                            setUser({ ...user, password: e.target.value })
+                        }
+                    />
+                    <input
+                        type="tel"
+                        className="py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Phone Number"
+                        value={phone_number}
+                        onChange={(e) =>
+                            setUser({ ...user, phone_number: e.target.value })
+                        }
+                    />
+                   <input
+                        type="text"
+                        className="py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Type"
+                        value={type}
+                        onChange={(e) =>
+                            setUser({ ...user, type: e.target.value })
+                        }
+                    />
+                    <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                        Add
+                    </button>
+                </form>
+            </div>
+        </Fragment>
+    )
+}
 
-  return (
-    <Fragment>
-      <h1 className="text-center font-bold text-4xl">High Ground App</h1>
-      <div className="flex justify-center items-center">
-      <form className="flex w-[35%] justify-center my-4" onSubmit={onSubmitForm}>
-        
-        <input
-          type="text"
-          className="py-2 px-3 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 flex-1"
-          placeholder="Enter text here" value={user} onChange={e => setUser(e.target.value)}
-        />
-        <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-          Add
-        </button>
-      </form>
-      </div>
-    </Fragment>
-  );
-};
-
-export default InputDashboard;
+export default InputDashboard
