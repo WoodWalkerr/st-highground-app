705174bfc9cb4107a8250f49a3e9ff8e91120742
Melven Bagolaque <melvenbagolaque@gmail.com>
2023-04-09 23:08:23 +0800

    feature: updated server-side

--- server/server.js
+++ server/server.js
@@ -23,29 +23,27 @@ app.get('/api/v1/users', (req, res) => {
     usersController.getUsers().then((data) => res.json(data))
 })
 
-app.post("/api/v1/users", (req, res) => {
-    usersController.createUser(req.body.user).then((data) => res.json(data));
-  });
+app.post('/api/v1/users', (req, res) => {
+    usersController.createUser(req.body.user).then((data) => res.json(data))
+})
 
-  app.put('/api/v1/users/:id', (req, res) => {
-    const { id } = req.params;
-    const user = req.body;
-  
-    usersController.updateUser({ ...user, id })
-      .then((data) => res.json(data))
-      .catch((error) => {
-        console.log('Error:', error);
-        res.status(500).send('Server error!');
-      });
-  });
-  
-app.delete('/api/v1/users/:id', (req,res) => {
+app.put('/api/v1/users/:id', (req, res) => {
+    const { id } = req.params
+    const user = req.body
+
+    usersController
+        .updateUser({ ...user, id })
+        .then((data) => res.json(data))
+        .catch((error) => {
+            console.log('Error:', error)
+            res.status(500).send('Server error!')
+        })
+})
+
+app.delete('/api/v1/users/:id', (req, res) => {
     usersController.deleteUser(req.params.id).then((data) => res.json(data))
 })
 
 app.listen(port, () => {
     console.log(`Server listening on the port: ${port}`)
 })
-
-
-
